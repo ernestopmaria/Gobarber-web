2c19e35d6cdae388d0b1862a56ca2f8d93cd00d4
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-07-17 21:29:41 +0100

    tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Signin from './pages/Signin/index';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Hellow World</h1>;
+const App: React.FC = () => (
+  <>
+    <Signin />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
