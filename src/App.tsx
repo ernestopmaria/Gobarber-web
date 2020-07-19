0a1d35b450ac35e018cc33ee2e9c668c4ecf6ce3
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-07-19 21:58:22 +0100

    callbacks e useEffects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Signin from './pages/Signin/index';
+// import Signin from './pages/Signin/index';
+import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <Signin />
+    <SignUp />
     <GlobalStyle />
   </>
 );
