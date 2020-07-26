eff3208d33203813670d72f731d93721a4c7da3b
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-07-26 20:38:22 +0100

    adicionando o toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import SignIn from './pages/SignIn/index';
 import GlobalStyle from './styles/global';
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer/index';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -36,8 +36,10 @@ const SignIn: React.FC = () => {
         });
         await schema.validate(data, { abortEarly: false });
       } catch (err) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],
