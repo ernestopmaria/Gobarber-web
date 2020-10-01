5202b0b80a3e64802da278f7899af541d68e70b6
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-10-01 10:28:14 +0100

    forgot password

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -1,6 +1,6 @@
 import { FormHandles } from '@unform/core';
 import React, { useRef, useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -11,6 +11,7 @@ import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 
 import { useToast } from '../../hooks/toast';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -22,6 +23,7 @@ const SignIn: React.FC = () => {
 
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -36,6 +38,18 @@ const SignIn: React.FC = () => {
           ),
         });
         await schema.validate(data, { abortEarly: false });
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
 
         history.push('/');
       } catch (err) {
@@ -51,7 +65,7 @@ const SignIn: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location.search],
   );
   return (
     <Container>
