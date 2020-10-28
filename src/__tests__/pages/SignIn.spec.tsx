264fee73217deaf67907475283a037473cec87f8
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-10-28 21:56:52 +0000

    Create SignIn.spec.tsx

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import SignIn from '../../pages/SignIn';
+
+describe('SignIn Page', () => {
+  it('should be able to sign in', () => {
+    const { debug } = render(<SignIn />);
+
+    debug();
+  });
+});
